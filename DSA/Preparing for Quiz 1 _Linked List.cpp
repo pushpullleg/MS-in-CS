2b2b5e24a4135a25a46caf12384b91f21a6756f4
pushpullleg <mravichandr@leomail.tamuc.edu>
2025-02-12 21:48:25 -0600

    coming back home and working on again

--- DSA/Preparing for Quiz 1 _Linked List.cpp
+++ DSA/Preparing for Quiz 1 _Linked List.cpp
@@ -1,3 +1,17 @@
 /*
 There is a exam tomorrow and i am working on it 
+
+Learning about type default
 */
+
+/*
+Write a function allLessThan(link h, int value) that checks if all the 
+values in the linked list are less than a given value. The function should 
+return 1 if all values are less than the given value, and 0 otherwise.
+*/
+
+int allLessThan(link h, int value) {
+    link t=h;
+    
+
+}
