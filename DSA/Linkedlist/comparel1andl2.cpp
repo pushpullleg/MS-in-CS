a4d5d06136daffcf893a8e79424bfa9ad199015d
pushpullleg <mravichandr@leomail.tamuc.edu>
2025-02-20 19:51:58 -0600

    Add function to output items in linked list 1 not present in linked list 2

---
+++ DSA/Linkedlist/comparel1andl2.cpp
@@ -0,0 +1,25 @@
+/*this is the example no.11 tejaswini class notes
+output all the items in the linkedlist 1 but not in l2
+assuming that both the linkedlist are sorted in order 
+
+I have a sub question came to my mind , arrange and return 
+linkedlist in sorting 
+*/
+
+void diff(link l1,link l2){
+    link t1=l1;
+    link t2=l2;
+    while(t1!=0 && t2!=0){
+        if(t1->item==t2->item){
+            t1=t1->next;
+            t2=t2->next;
+        }
+        if(t1->item<t2->item){
+            t1=t1->next;
+        }else{
+            cout<<t1->item;
+            t1=t1->next;
+            t2=t2->next;
+        }
+    }
+}
